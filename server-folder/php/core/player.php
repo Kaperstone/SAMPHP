5a52e56daf5a6f9ba0fabfa13b44309ad3b88f43
Kirill <Kaperstone@users.noreply.github.com>
2015-02-12 20:59:11 +0200

    Update player.php

--- server-folder/php/core/player.php
+++ server-folder/php/core/player.php
@@ -728,15 +728,7 @@ class Player
 	public function geoInfo($ip = NULL, $purpose = "location", $deep_detect = TRUE) {
 	    $output = NULL;
 	    if(filter_var($ip, FILTER_VALIDATE_IP) === FALSE) {
-	        $ip = $_SERVER["REMOTE_ADDR"];
-	        if($deep_detect) {
-			if(filter_var(@$_SERVER['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP)) {
-				$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-			}
-			if(filter_var(@$_SERVER['HTTP_CLIENT_IP'], FILTER_VALIDATE_IP)) {
-				$ip = $_SERVER['HTTP_CLIENT_IP'];
-			}
-	        }
+	        $ip=GetPlayerIp($this->id);
 	    }
 	    $purpose    = str_replace(array("name", "\n", "\t", " ", "-", "_"), NULL, strtolower(trim($purpose)));
 	    $support    = array("country", "countrycode", "state", "region", "city", "location", "address");
